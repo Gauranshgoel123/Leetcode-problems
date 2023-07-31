c89de1b1e85e5c638b6a50691838f4f30e34b9e6
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2023-08-01 03:01:44 +0530

    Update valid parenthesis.py
    Alternate solution

--- valid parenthesis.py
+++ valid parenthesis.py
@@ -1,20 +1,18 @@
-lass Solution(object):
+class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        # Create a pair of opening and closing parrenthesis...
+        opcl = dict(('()', '[]', '{}'))
+        # Create stack data structure...
         stack = []
-        # return empty string '' when asked with invalid char
-        paren_dict = {')': '(', '}':'{', ']': '['}
-        for i in s:
-            # if c is a right parenthesis
-            if i in paren_dict:
-                # return False if there is no matching left parenthesis
-                if not stack or stack.pop() != paren_dict[i]:
-                    return False
-            # push left parenthesis(assume only left and right parenthesis is in s)
-            else:
-                stack.append(i)
-        # To ensure all opening parenthesis was matched
-        return not stack
+        # Traverse each charater in input string...
+        for idx in s:
+            # If open parentheses are present, append it to stack...
+            if idx in '([{':
+                stack.append(idx)
+            # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            # If not, we need to return false...
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        # At last, we check if the stack is empty or not...
+        # If the stack is empty it means every opened parenthesis is being closed and we can return true, otherwise we return false...
+        return len(stack) == 0
