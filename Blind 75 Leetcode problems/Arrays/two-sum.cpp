e1f046f3093fe52c33ed8e69d2ca6f524a16bf66
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-02-11 16:32:40 +0530

    Update two-sum.cpp
    Better answer updated for this question https://leetcode.com/problems/two-sum/description/
    unordered map i.e hash table is used to check if the subtraction of current and target exists in the hash table.
    must see once

--- Blind 75 Leetcode problems/Arrays/two-sum.cpp
+++ Blind 75 Leetcode problems/Arrays/two-sum.cpp
@@ -1,3 +1,4 @@
+//BRUTE FORCE APPROACH COMPARATIVE HIGHER RUNTIME
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -7,6 +8,27 @@ public:
                     return {i,j};
                 }
             }
+        }
+        return {};
+    }
+};
+
+//HASH TABLE OPTIMIZED APPROACH
+//RUM TIME 4MS
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap; 
+        //We will select the element of the array one by one using a loop(say i).
+        for (int i = 0; i < nums.size(); i++) {
+            //Then we will check if the other required element(i.e. target-arr[i]) exists in the hashMap.
+            int complement = target - nums[i];
+            //f that element exists, then we will return “YES” for the first variant or we will return the current index i.e. i, and the index of the element found using map i.e. mp[target-arr[i]]
+            if (numMap.find(complement) != numMap.end()) {/*means complement exists*/
+                return {numMap[complement], i};
+            }
+            //If that element does not exist, then we will just store the current element in the hashMap along with its index. Because in the future, the current element might be a part of our answer.
+            numMap[nums[i]] = i;
         }
         return {};
     }
