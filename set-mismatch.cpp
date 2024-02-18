0b80ac9af81c4f1b5d6c3fbecba232eb03bca937
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-02-18 20:21:34 +0530

    Update set-mismatch.cpp

--- set-mismatch.cpp
+++ set-mismatch.cpp
@@ -1,8 +1,33 @@
-// Calculate Expected Sum:
+//Brute force
+// class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& nums) {
+//         //Brute Force
+//         //Initialize variables dup and missing to -1. These will be used to store the duplicate and missing numbers.
+//         int dup=-1;
+//         int missing=-1;
+//         for(int i=1;i<=nums.size();i++){
+//             int count=0;
+//             for (int j=0;j<nums.size();j++){
+//                 if(nums[j]==i){
+//                     count++;
+//                 }
+//             }
+//             if(count==2){
+//                 dup=i;
+//             }
+//             else if( count ==0){
+//                 missing=i;
+//             }
+//         }
+//         return {dup,missing};
+//     }
+// };
 
+
+// Calculate Expected Sum:
 // Calculate the expected sum of numbers from 1 to n using the formula (n * (n + 1)) / 2. This assumes no duplicates and no missing numbers.
 // Calculate Array and Unique Sums:
-
 // Calculate the sum of all elements in the array (array_sum).
 // Use an unordered_set (s) to get the unique elements in the array and calculate their sum (unique_sum).
 // Find Missing and Duplicate:
