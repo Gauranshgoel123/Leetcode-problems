cb95b6c2c6f5ffc838bdbd12b8c1e8695a95c6d2
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-02-24 22:42:36 +0530

    Update find-polygon-with-the-largest-perimeter.java

--- find-polygon-with-the-largest-perimeter.java
+++ find-polygon-with-the-largest-perimeter.java
@@ -1,3 +1,38 @@
+// Approach 1( With Backward Iteration)
+// Sum all the elements in the nums array.
+// Traverse the array backwards.
+// Check if the sum minus the current element is greater than the current element.
+// If the condition is met, return the sum plus the current element as the perimeter.
+// If no suitable perimeter is found, return -1.
+// Complexity
+// Time complexity:
+// O(nlogn)O(nlogn)O(nlogn)
+
+// Space complexity:
+// O(1)O(1)O(1)
+
+// Code
+
+
+// class Solution {
+//     public long largestPerimeter(int[] nums) {
+//         long sum = 0;
+//         Arrays.sort(nums);
+//         for (int num : nums) {
+//             sum += num;
+//         }
+//         int n = nums.length;
+//         for (int i = n - 1; i >= 2; i--) {
+//             sum -= nums[i];
+//             if (sum > nums[i]) {
+//                 return sum + nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
 // Approach 2 ( With Forward Iteration )
 // Sort the array: Start by sorting the array of numbers in ascending order.
 
