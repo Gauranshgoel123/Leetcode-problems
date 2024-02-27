b1cc45afae7686232e50a14af6666e5ee49c02be
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-02-27 16:05:54 +0530

    Update left-and-right-sum-differences.java

--- left-and-right-sum-differences.java
+++ left-and-right-sum-differences.java
@@ -1,3 +1,27 @@
+// class Solution {
+//     public int[] leftRightDifference(int[] nums) {
+//         int n = nums.length;
+//         int[] answer = new int[n];
+//         int[] leftSum = new int[n];
+//         int[] rightSum = new int[n];
+//         int left = 0;
+//         int right = n-1; 
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<=i-1;j++){
+//                 leftSum[i]+=nums[j];
+//             }
+//             for(int j=i+1;j<=n-1;j++){
+//                 rightSum[i]+=nums[j];
+//             }
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(leftSum[i]>rightSum[i]) answer[i]=leftSum[i]-rightSum[i];
+//             else answer[i]=rightSum[i]-leftSum[i];
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int n = nums.length;
