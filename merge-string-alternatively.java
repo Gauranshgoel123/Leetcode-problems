ff57c4ddc873d67a3711d94f88bffed820bfde49
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-03-03 20:45:41 +0530

    easy java good understanding loop pointer question
    You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
    
    Return the merged string.
    
     
    
    Example 1:
    
    Input: word1 = "abc", word2 = "pqr"
    Output: "apbqcr"
    Explanation: The merged string will be merged as so:
    word1:  a   b   c
    word2:    p   q   r
    merged: a p b q c r
    Example 2:
    
    Input: word1 = "ab", word2 = "pqrs"
    Output: "apbqrs"
    Explanation: Notice that as word2 is longer, "rs" is appended to the end.
    word1:  a   b 
    word2:    p   q   r   s
    merged: a p b q   r   s
    Example 3:
    
    Input: word1 = "abcd", word2 = "pq"
    Output: "apbqcd"
    Explanation: Notice that as word1 is longer, "cd" is appended to the end.
    word1:  a   b   c   d
    word2:    p   q 
    merged: a p b q c   d
     
    
    Constraints:
    
    1 <= word1.length, word2.length <= 100
    word1 and word2 consist of lowercase English letters.

---
+++ merge-string-alternatively.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+        StringBuilder res=new StringBuilder() ;
+        while(i<word1.length() || i<word2.length()){
+            if(i<word1.length()){
+                res.append(word1.charAt(i));
+            }
+            if(i<word2.length()){
+                res.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return res.toString();
+    }
+}
