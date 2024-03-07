02fa0e95add05fee06b8ac2e196bff39ad305b37
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-03-07 16:41:30 +0530

    slow and fast pointers java easy linked list
    Given the head of a singly linked list, return the middle node of the linked list.
    
    If there are two middle nodes, return the second middle node.
    
     
    
    Example 1:
    
    
    Input: head = [1,2,3,4,5]
    Output: [3,4,5]
    Explanation: The middle node of the list is node 3.
    Example 2:
    
    
    Input: head = [1,2,3,4,5,6]
    Output: [4,5,6]
    Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
     
    
    Constraints:
    
    The number of nodes in the list is in the range [1, 100].
    1 <= Node.val <= 100

---
+++ middle-of-the-linked-list.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(fast.next!=null){
+            return slow.next;
+        }
+        else{   
+            return slow;
+        }
+    }
+}
