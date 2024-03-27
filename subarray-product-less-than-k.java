7bd160c18da5438a728b08f7f8f1bd6d118ce1b8
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-03-27 15:56:49 +0530

    713 medium java sliding window
    Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
    
     
    
    Example 1:
    
    Input: nums = [10,5,2,6], k = 100
    Output: 8
    Explanation: The 8 subarrays that have product less than 100 are:
    [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
    Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
    Example 2:
    
    Input: nums = [1,2,3], k = 0
    Output: 0
     
    
    Constraints:
    
    1 <= nums.length <= 3 * 104
    1 <= nums[i] <= 1000
    0 <= k <= 106

--- subarray-product-less-than-k.java
+++ subarray-product-less-than-k.java
@@ -1,17 +1,15 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0;
-
-        int left = 0, right = 0, product = 1, count = 0;
+        int left = 0,  product = 1, count = 0;
         int n = nums.length;
-
-        while (right < n) {
-            product *= nums[right];
-            while (product >= k) product /= nums[left++];
-            count += 1 + (right - left);
-            right++;
+        for(int right=0;right<n;right++){
+            product*=nums[right];
+            while(product>=k && left<=right){
+                product/=nums[left];
+                left++;
+            }
+            count+=right-left+1;
         }
-
         return count;
     }
 }
