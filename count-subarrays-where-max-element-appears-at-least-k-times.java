59512a75d344139e82d6b81e6f1ddb9939d91814
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-03-29 18:11:10 +0530

    2962 medium java sliding window subarray max element freq good

--- count-subarrays-where-max-element-appears-at-least-k-times.java
+++ count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,50 +1,22 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        long res = 0;
-        int max = 0, count = 0, n = nums.length;
-        // Find the maximum element in the array
-        for (int num : nums)
-            max = Math.max(max, num);
-        int l = 0;
-        for (int r = 0; r < n; r++) {
-            if (nums[r] == max)
-                count++;
-            // If count is greater than or equal to k, calculate subarrays count
-            while (count >= k) {
-                res += n - r;
-                if (nums[l] == max)
-                    count--;
-                l++;
-            }
+        int start=0,maxx=0,maxfreq=0;
+        long ans=0;
+        for(int num:nums){
+           maxx=Math.max(maxx,num);
         }
-        return res;
+        for(int end=0;end<nums.length;end++){
+            if(nums[end]==maxx){
+                maxfreq++;
+            }
+            while(maxfreq==k){
+                if(nums[start]==maxx){
+                    maxfreq--;
+                }
+                start++;
+            }
+            ans+=start;
+        }
+        return ans;
     }
 }
-
-
-
-// class Solution {
-//     public long countSubarrays(int[] nums, int k) {
-        
-//         int start=0;
-//         long ans=0;
-//         int maxx=0;
-//         int maxfreq=0;
-//         for(int num:nums){
-//            maxx=Math.max(maxx,num);
-//         }
-//         for(int num:nums){
-//             if(num==maxx){
-//                 maxfreq++;
-//             }
-//             while(maxfreq==k){
-//                 if(nums[start]==maxx){
-//                     maxfreq--;
-//                 }
-//                 start++;
-//             }
-//             ans+=start;
-//         }
-//         return ans;
-//     }
-// }
