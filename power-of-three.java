e41222f51b4dd681dcd71bbc249bbe1e4e2acce3
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-05-19 15:44:35 +0530

    326 easy java recursion math good
    Given an integer n, return true if it is a power of three. Otherwise, return false.
    
    An integer n is a power of three, if there exists an integer x such that n == 3x.
    
     
    
    Example 1:
    
    Input: n = 27
    Output: true
    Explanation: 27 = 33
    Example 2:
    
    Input: n = 0
    Output: false
    Explanation: There is no x where 3x = 0.
    Example 3:
    
    Input: n = -1
    Output: false
    Explanation: There is no x where 3x = (-1).

---
+++ power-of-three.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if( n==1 ) return true;
+        if(n % 3 != 0 || n==0){
+            return false;
+        }
+        return isPowerOfThree(n/3);
+        
+    }
+}
+
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+//         if(n==-1 || n==0) return false;
+//        while(n%3==0){n=n/3;}
+//        if(n==1)
+//        return true;
+//        else
+//        return false;
+//     }
+// }
