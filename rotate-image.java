0ce63bf822bd7a9a8cef2d9b289298cf2b6bbc9a
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-07-22 01:01:39 +0530

    medium java striver 48 transpose matrix reverse each row
    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
    
    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
    
     
    
    Example 1:
    
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [[7,4,1],[8,5,2],[9,6,3]]
    Example 2:
    
    
    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
     
    
    Constraints:
    
    n == matrix.length == matrix[i].length
    1 <= n <= 20
    -1000 <= matrix[i][j] <= 1000
    
    Seen this question in a real interview before?
    1/5
    Yes
    No
    Accepted
    1.8M
    Submissions
    2.4M
    Acceptance Rate
    75.2%
    Topics
    Array
    Math
    Matrix

--- rotate-image.java
+++ rotate-image.java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public void rotate(int[][] matrix) {
+        //brute force not in place
+        // int n=matrix.length;
+        // int[][] ans=new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<matrix[i].length;j++){
+        //         ans[j][n-1-i]=matrix[i][j];
+        //     }
+        // }
+        // return ans;
+        //transpose
+        int n=matrix.length;
+        int temp=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //reverse each row
+        for(int i=0;i<n;i++){
+            int start = 0;
+            int end = n-1;
+            while (start < end) {
+                int temp1 = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp1;
+                start++;
+                end--;
+            }
+        }
+    }
+}
