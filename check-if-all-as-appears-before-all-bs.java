7ff91a9804ac57946e453460ee63afe8acab4f80
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-08-24 18:25:06 +0530

    v easy string
    Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
    
     
    
    Example 1:
    
    Input: s = "aaabbb"
    Output: true
    Explanation:
    The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
    Hence, every 'a' appears before every 'b' and we return true.
    Example 2:
    
    Input: s = "abab"
    Output: false
    Explanation:
    There is an 'a' at index 2 and a 'b' at index 1.
    Hence, not every 'a' appears before every 'b' and we return false.
    Example 3:
    
    Input: s = "bbb"
    Output: true
    Explanation:
    There are no 'a's, hence, every 'a' appears before every 'b' and we return true.
     
    
    Constraints:
    
    1 <= s.length <= 100
    s[i] is either 'a' or 'b'.
    
    Seen this question in a real interview before?
    1/5
    Yes
    No
    Accepted
    86.8K
    Submissions
    120.3K
    Acceptance Rate
    72.1%
    Topics
    String
    Companies
    Hint 1
    You can check the opposite: check if there is a ‘b’ before an ‘a’. Then, negate and return that answer.
    Hint 2
    s should not have any occurrences of “ba” as a substring.

---
+++ check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkString(String s) {
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='b'&&s.charAt(i+1)=='a')
+            return false;
+        }
+        return true;
+    }
+}
