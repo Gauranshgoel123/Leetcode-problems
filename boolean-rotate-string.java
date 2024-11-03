5e31e8ddeaba30722fe36f46507a2cebfc387451
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-11-04 01:44:24 +0530

    check if goal in concatenated s+s
    796. Rotate String
    Solved
    Easy
    Topics
    Companies
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.
     
    
    Example 1:
    
    Input: s = "abcde", goal = "cdeab"
    Output: true
    Example 2:
    
    Input: s = "abcde", goal = "abced"
    Output: false
     
    
    Constraints:
    
    1 <= s.length, goal.length <= 100
    s and goal consist of lowercase English letters.
    
    Seen this question in a real interview before?
    1/5
    Yes
    No
    Accepted
    492.7K
    Submissions
    786.3K
    Acceptance Rate
    62.7%

---
+++ boolean-rotate-string.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()==goal.length() && (s+s).contains(goal)) return true;
+        return false;
+    }
+}
